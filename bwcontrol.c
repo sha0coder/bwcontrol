d103f9b3beeaaaaea8740c93cdcab405554a04b0
@sha0coder <sha0@badchecksum.net>
2015-06-10 00:28:47 +0200

    byte Kbyte and Mega precission on BW_LIMIT

--- bwcontrol.c
+++ bwcontrol.c
@@ -4,7 +4,7 @@
 
     USAGE:
         make
-        LD_PRELOAD=./bwcontrol.so BW_LIMIT=80 BW_TIMEOUT=100  hping3 -1 8.8.8.8  --fast
+        LD_PRELOAD=./bwcontrol.so BW_LIMIT=b80 BW_TIMEOUT=100  hping3 -1 8.8.8.8  --fast
 
         this hping has been limited his max speed to 80 bytes per second, and in 100 seconds will be finished.
 */
@@ -25,8 +25,9 @@ static ssize_t (*__libc_sendmsg)(int , const struct msghdr *, int);
 static void *libc = NULL;
 static int bw_bytes = 0;
 static time_t bw_time = NULL;
-static int bw_limit = 0;
+static float bw_limit = 0;
 static unsigned int bw_global_timeout = 0;
+
 
 
 ssize_t sendto(int sockfd, const void *buf, size_t len, int flags,
@@ -43,16 +44,31 @@ void __attribute__ ((constructor)) init(void) {
     if (getenv("BW_LIMIT") == NULL || getenv("BW_TIMEOUT") == NULL)
         usage();
 
+    char *env_bt_limit = getenv("BW_LIMIT");
+    char precission = env_bt_limit[0];
+    env_bt_limit++;
+
     bw_global_timeout = time(NULL) + atoi(getenv("BW_TIMEOUT"));
-    bw_limit = atoi(getenv("BW_LIMIT"));
+    bw_limit = atof(env_bt_limit);
     bw_time = time(NULL);
+
+    
+    switch (precission) {
+        case 'K':
+        case 'k': bw_limit *= 1024; break;
+        case 'M':
+        case 'm': bw_limit *= 1024*1024; break;
+        case 'G':
+        case 'g': bw_limit *= 1024*1024*1024; break;
+    }
+
+    printf("limit: %f bytes\n", bw_limit);
 
     __libc_send = dlsym(libc, "send");
     __libc_sendto = dlsym(libc, "sendto");
     __libc_sendmsg = dlsym(libc, "sendmsg");
 
     printf("BW Control loaded.\n");
-
 }
 
 
