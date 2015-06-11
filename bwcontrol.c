483c4caee8a9216b2c577d8b088a2d180467f00e
Jesus Olmos <jesus.olmos@innevis.lan>
2015-06-11 12:38:21 +0200

    performance change

--- bwcontrol.c
+++ bwcontrol.c
@@ -125,27 +125,23 @@ int bw_mustDrop() {
 ssize_t send(int sockfd, const void *buf, size_t len, int flags) {
     bw_bytes += len;
 
-    if (bw_mustDrop()) {
-        __libc_send(sockfd, buf, 0, flags);
+    if (bw_mustDrop()) 
         return len;
-    }
 
     
-    printf("bytes sent: %d\n",len);
+    //printf("bytes sent: %d\n",len);
     return __libc_send(sockfd, buf, len, flags);
 }
 
 ssize_t sendto(int sockfd, const void *buf, size_t len, int flags,
-                      const struct sockaddr *dest_addr, socklen_t addrlen) {
+                    const struct sockaddr *dest_addr, socklen_t addrlen) {
 
     bw_bytes += len;
 
-    if (bw_mustDrop()) {
-        __libc_sendto(sockfd,buf,0,flags,dest_addr,addrlen);
+    if (bw_mustDrop()) 
         return len;
-    }
 
-    printf("bytes sent: %d\n",len);
+    //printf("bytes sent: %d\n",len);
     return __libc_sendto(sockfd, buf, len, flags, dest_addr, addrlen);
 }
 
@@ -156,11 +152,10 @@ ssize_t sendmsg(int sockfd, const struct msghdr *msg, int flags) {
 
     bw_bytes += len;
 
-    if (bw_mustDrop()) {
+    if (bw_mustDrop()) 
         return len;
-    }
 
-    printf("sending %d bytes\n", len);
+    //printf("sending %d bytes\n", len);
     return __libc_sendmsg(sockfd, msg, flags);
 }
 
